5f72afbea6effa8f6e1b7b7e8bee6b8f561535ea
Ashwin Vemuganti <ashoo.vemu@gmail.com>
2021-11-12 00:16:53 +0000

    Changes made through Theia Tool for Final Project for HTML,CSS and JavaScript Course.

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    if(principal < 1){
+        alert("Enter a Positive Number");
+        document.getElementById("principal").focus;
+        document.getElementById("principal").style.color = "red";
+        return false;
+    }
+    var int_rate = document.getElementById("rate").value;
+    var no_of_years = document.getElementById("years").value;
+    var interest = principal * int_rate * no_of_years / 100;
+    var year = new Date().getFullYear() + parseInt(no_of_years);
+    document.getElementById("result").innerHTML = 'If you deposit ' +principal+', <br/> at an interest rate of '+int_rate+'.<br/> You will receive an amount of '+interest+',<br/>in the year '+year;
 }
-        
+
+function updateRate(){
+    int_rate = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = int_rate;
+}        
